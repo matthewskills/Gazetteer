65c3ef6b2ad42f63a1cd4efa6b55eaf4017ea3d9
Matt Skills <66142050+matthewskills@users.noreply.github.com>
2021-02-03 18:56:55 +0000

    Update poi.php

--- app/data/poi.php
+++ app/data/poi.php
@@ -16,7 +16,7 @@ function getPoi($maxLat,$maxLng,$minLat,$minLng) {
 
 function getRailStations($maxLat,$maxLng,$minLat,$minLng) {
 
-        $url = 'http://api.opentripmap.com/0.1/en/places/bbox?lon_min='.$minLng.'&lat_min='.$minLat.'&lon_max='.$maxLng.'&lat_max='.$maxLat.'&kinds=railway_stations&format=geojson&apikey=5ae2e3f221c38a28845f05b6b681cb9fc12ad7d4162a7319a641dc78';
+        $url = 'http://api.opentripmap.com/0.1/en/places/bbox?lon_min='.$minLng.'&lat_min='.$minLat.'&lon_max='.$maxLng.'&lat_max='.$maxLat.'&kinds=railway_stations&format=geojson&apikey=##########';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -30,7 +30,7 @@ function getRailStations($maxLat,$maxLng,$minLat,$minLng) {
 
 function getHotels($maxLat,$maxLng,$minLat,$minLng) {
 
-        $url = 'http://api.opentripmap.com/0.1/en/places/bbox?lon_min='.$minLng.'&lat_min='.$minLat.'&lon_max='.$maxLng.'&lat_max='.$maxLat.'&kinds=other_hotels&format=geojson&apikey=5ae2e3f221c38a28845f05b6b681cb9fc12ad7d4162a7319a641dc78';
+        $url = 'http://api.opentripmap.com/0.1/en/places/bbox?lon_min='.$minLng.'&lat_min='.$minLat.'&lon_max='.$maxLng.'&lat_max='.$maxLat.'&kinds=other_hotels&format=geojson&apikey=#########';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -44,7 +44,7 @@ function getHotels($maxLat,$maxLng,$minLat,$minLng) {
 
 function getTourist($maxLat,$maxLng,$minLat,$minLng) {
 
-        $url = 'http://api.opentripmap.com/0.1/en/places/bbox?lon_min='.$minLng.'&lat_min='.$minLat.'&lon_max='.$maxLng.'&lat_max='.$maxLat.'&kinds=tourist_object&format=geojson&apikey=5ae2e3f221c38a28845f05b6b681cb9fc12ad7d4162a7319a641dc78';
+        $url = 'http://api.opentripmap.com/0.1/en/places/bbox?lon_min='.$minLng.'&lat_min='.$minLat.'&lon_max='.$maxLng.'&lat_max='.$maxLat.'&kinds=tourist_object&format=geojson&apikey=###########';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
