3c40f1107c35391652061b98c1eac372ff7a7bf7
Matt Skills <66142050+matthewskills@users.noreply.github.com>
2021-02-03 18:57:16 +0000

    Update newsProviders.php

--- app/data/newsProviders.php
+++ app/data/newsProviders.php
@@ -2,7 +2,7 @@
 
 function getNewsProviders($isoa2) {
 
-    $url = 'https://newsapi.org/v2/sources?language=en&country='.$isoa2.'&apiKey=f078987008884f8aa27e5b2a38dca08d';
+    $url = 'https://newsapi.org/v2/sources?language=en&country='.$isoa2.'&apiKey=########';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
